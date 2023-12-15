8e41614610f884fa8974a285b24da66e49cd48c1
Moncef <75338118+Mushrimpy@users.noreply.github.com>
2023-12-15 22:35:16 +0100

    Create day-01.rs

---
+++ day-01.rs
@@ -0,0 +1,39 @@
+use std::fs;
+
+fn part1(input: &str) -> u32 {
+    let mut total = 0;
+    for line in input.lines() {
+        let nums: Vec<u32> = line
+                .chars()
+                .filter_map(|ch| ch.to_digit(10))
+                .collect();
+
+        total += (nums.first().unwrap()) * 10 + nums.last().unwrap();
+    }
+    total
+}  
+
+fn part2(input: &str) -> u32 {
+    let changes = vec![("one", "one1one"), 
+    ("two", "two2two"), 
+    ("three", "three3three"), 
+    ("four", "four4four"), 
+    ("five", "five5five"), 
+    ("six", "six6six"), 
+    ("seven", "seven7seven"), 
+    ("eight", "eight8eight")];
+
+    let mut input = input.to_string();
+
+    for (from, to) in changes {
+        input = input.replace(from, to);
+    }
+    part1(&input) 
+} 
+
+
+fn main() {
+    let input = fs::read_to_string("./input.txt").unwrap();
+    println!("Part 1: {} \nPart 2: {}", part1(&input), 0);    
+}
+
